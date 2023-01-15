6a1494e933fb270c88a52f118ae4909aaecb26cb
Jason Hirata <Jason.Hirata@us.kbr.com>
2023-01-15 12:29:10 -1000

    updating permissions to graph ql queries

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import { Amplify, API, graphqlOperation } from 'aws-amplify'
+import { Amplify, API, graphqlOperation, Auth } from 'aws-amplify'
+import { CognitoHostedUIIdentityProvider } from '@aws-amplify/auth';
 import awsconfig from './aws-exports'
 import { getTeam, listTeams } from './graphql/queries'
 
@@ -8,9 +9,23 @@ import { getTeam, listTeams } from './graphql/queries'
 Amplify.configure(awsconfig)
 
 function App() {
-  API.graphql(graphqlOperation(getTeam, {
+  API.graphql(graphqlOperation(listTeams, {
 
   }))
+    .then(data => {
+      console.log(data)
+    })
+    .catch(err => {
+      console.error(err)
+    })
+  
+  const setUser = user => {
+    console.log(user)
+  }
+
+   Auth.currentAuthenticatedUser()
+      .then(currentUser => setUser(currentUser))
+      .catch(() => console.log("Not signed in"));
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +33,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={() => Auth.federatedSignIn({
+
+          provider: CognitoHostedUIIdentityProvider.Google
+        })}>Login</button>
         <a
           className="App-link"
           href="https://reactjs.org"
