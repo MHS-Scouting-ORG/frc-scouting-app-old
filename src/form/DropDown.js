9f75961b96a64ba83fde28d8f5be10a835dd6ee9
dylandomingo57@ <dylandomingo57@gmail.com>
2023-02-04 15:03:55 -1000

    added options param

--- src/form/DropDown.js
+++ src/form/DropDown.js
@@ -13,8 +13,9 @@ class DropDown extends React.Component{
     render(){
         return(
             <div>
-                <select onChange={this.changeDropsDownState}>
-                    <option></option>
+                <select onChange={this.dropDownChange}>
+                    <option key={"empty"}></option>
+                    {this.props.options.map((option) => <option key={option}>{option}</option>)}
                 </select>
             </div>
         )
