037925dad19f6f10ddc503f65b2314a489ebf7a5
dylandomingo57@ <dylandomingo57@gmail.com>
2023-02-04 16:02:16 -1000

    edited timer, not functional yet however

--- src/form/StationTimer.js
+++ src/form/StationTimer.js
@@ -3,21 +3,39 @@ import React from "react";
 class StationTimer extends React.Component{
     constructor(props){
         super(props);
+        this.state = {
+            counter: 0,
+            startTime: false
+        }
     }
 
-    starterTimer(){
-
+    startTimer(){
+        console.log("starting")
+        if (!this.state.startTime) {
+            setInterval(function() {
+                let currCount = this.state.count; //ERROR: Uncaught TypeError: Cannot read properties of undefined (reading 'count')
+                this.setState({
+                    counter: currCount + .1,
+                });
+            }, 100);
+        }
     }
 
     endTimer(){
-
+        console.log("ending");
+        this.setState(({
+            startTime: true
+        }));
     }
 
     render(){
         return(
             <div>
-                
+                <input value={this.state.ms}/>
+                <button onClick={() => (!this.state.startTime) ? this.startTimer() : this.endTimer()}>Start/Stop</button>
             </div>
         )
     }
 }
+
+export default StationTimer;
