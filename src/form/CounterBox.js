f931847115f68c9814fbbcb360d03f3557200480
dylandomingo57@ <dylandomingo57@gmail.com>
2023-02-10 18:00:45 -1000

    timer still not functional, updated counterbox

--- src/form/CounterBox.js
+++ src/form/CounterBox.js
@@ -3,11 +3,14 @@ import React from 'react';
 class CounterBox extends React.Component{
     constructor(props) {
         super(props);
+        this.updateCounter = this.updateCounter.bind(this);
         this.state = {
             counter: 0
         }
     }
-    
+
+
+
     updateCounter(increment) {
         let val = increment + this.state.counter;
         this.setState((state) => ({
@@ -18,8 +21,9 @@ class CounterBox extends React.Component{
     render() {
         return(
             <div>
+                <label>{this.props.title}</label>
                 <button onClick={() => this.updateCounter(-1)}>-</button>
-                <input value={this.state.counter}/>
+                <input type="number" value={this.state.counter}/>
                 <button onClick={() => this.updateCounter(1)}>+</button>
             </div>
         );
--- src/form/StationTimer.js
+++ src/form/StationTimer.js
@@ -3,36 +3,43 @@ import React from "react";
 class StationTimer extends React.Component{
     constructor(props){
         super(props);
+        this.startTimer = this.startTimer.bind(this)
+        this.endTimer = this.endTimer.bind(this)
         this.state = {
             counter: 0,
-            startTime: false
+            startTime: false,
+            pressed: false
         }
     }
 
     startTimer(){
         console.log("starting")
-        if (!this.state.startTime) {
+        this.setState({pressed: true})
+        if (this.state.startTime === false) {
+            let count = 0;
             setInterval(function() {
-                let currCount = this.state.counter; //ERROR: Uncaught TypeError: Cannot read properties of undefined (reading 'count')
-                this.setState({
-                    counter: currCount + .1,
-                });
+                count += .1
+                console.log(Math.round(count * 10) / 10);
             }, 100);
+            this.setState((state) => ({
+                counter: state.counter + Math.round(count * 10) / 10
+            }));
+        } 
+        else {
+            return;
         }
     }
 
     endTimer(){
         console.log("ending");
-        this.setState(({
-            startTime: true
-        }));
+        this.setState({startTime: true, pressed: false});
     }
 
     render(){
         return(
             <div>
                 <input value={this.state.counter}/>
-                <button onClick={() => (!this.state.startTime) ? this.startTimer() : this.endTimer()}>Start/Stop</button>
+                <button onClick={() => (this.state.pressed === false) ? this.startTimer() : this.endTimer()}>Start/Stop</button>
             </div>
         )
     }
