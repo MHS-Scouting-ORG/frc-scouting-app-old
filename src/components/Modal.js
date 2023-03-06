15e1bfa730d33ff52ecbb9400b01361ec1b8b720
Jason Hirata <abstractalchemist@gmail.com>
2023-03-05 16:20:21 -1000

    updating modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import Form from '../form/Form';
 
-class Modal extends React.Component{
-    constructor(props){
+class Modal extends React.Component {
+    constructor(props) {
         super(props);
     }
-    render(){
-        if(this.props.onOff){
-            const data = this.props.data[0]
-            console.log(data)
-            return(
-                <div className="modal">
-                    <Form regional={this.props.regional} matchData={data}></Form>
-                    <button onClick={() => this.props.offFunction()}> FINISH EDIT </button>
-                </div>
-            )
-        }
+    render() {
+        const formProps = { regional : this.props.regional }
+        if(this.props.data)
+            formProps.matchData =  this.props.data[0]
+        return (
+            <div className="modal" style={{ display: this.props.onOff ? "block" : "none" }}>
+                <Form {...formProps}></Form>
+                <button onClick={() => this.props.offFunction()}> FINISH EDIT </button>
+            </div>
+        )
     }
 }
 
