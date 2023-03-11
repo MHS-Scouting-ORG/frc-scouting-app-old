867529b4e3c5d84c4419c747c62f8df9d6b912be
rhysOcana <rhysdhustin.ocana2023@mauihigh.org>
2023-03-10 18:03:22 -1000

    fix match drop down

--- src/form/Form.js
+++ src/form/Form.js
@@ -322,7 +322,7 @@ class Form extends React.Component {
     if(matchTypeState === 'q'){
       matchState = "Qualification";
     } else if(matchTypeState === 'qf'){
-      matchState = "QuarterFinal";
+      matchState = "QuarterFinals";
     } else if(matchTypeState === 'sf'){
       matchState = "SemiFinal";
     } else if(matchTypeState === 'f'){
